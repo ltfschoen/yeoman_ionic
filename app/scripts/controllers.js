44282e9103a4219098a69aa887f2ee4c795655d4
ltfschoen <ltfschoen@gmail.com>
2014-09-14 08:52:51 +1000

    Added String Annotations for Dependencies to overcome Injection Minify problem

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,18 +2,25 @@
 
 angular.module('YeomanIonic.controllers', [])
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
+// AngularJS looks up its built-in services (prefixed with $) provided as arguments
+// as dependencies when controller constructed. Dependency Injection (DI) applies
+// the singleton instance of $http service to controller component
+// DI checks if Service Instance of $http already instantiated
+// else otherwise use Service Factory method fn for $http to construct it.
+// inline annotation array of services to inject for corresponding parameter 
+// to overcome problem with dependencies otherwise being minified incorrectly
+.controller('MapCtrl', ['$scope', '$ionicLoading', '$http', function($scope, $ionicLoading, $http) {
 
-  $scope.sentences = [
-    {'name': "Hello",
-     'snippet': "1"},
-    {'name': "Goodbye", 
-     'snippet': "2"},
-    {'name': "Greetings", 
-     'snippet': "3"},
-    {'name': "Cheerio", 
-     'snippet': "4"}
-  ];
+  // $http service for HTTP request to fetch data from web server asyncronously
+  // AngularJS detects and parses the JSON response automatically
+  // $http service returns a promise object with a success method
+  // assign dataset to scope of the controller as a Model named 'sentences'
+  // Shortcut method applied below
+  $http.get('data/sentences.json').success(function(data) {
+    $scope.sentences = data;
+  });
+
+  // dataset moved to data/sentences.json
 
   // default value of orderProp set so initialised in drop-down when used
   $scope.orderProp = 'snippet';
@@ -41,4 +48,4 @@ angular.module('YeomanIonic.controllers', [])
       alert('Unable to get location: ' + error.message);
     });
   };
-});
+}]);
