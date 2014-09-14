0dc6da555116dbccd38c5c101ca2033bf00604a5
ltfschoen <ltfschoen@gmail.com>
2014-09-15 07:55:18 +1000

    Fixed Splice method on Data to limit to 3 sentences

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -33,6 +33,9 @@ angular.module('YeomanIonic.controllers', [])
       // $scope.sentences = angular.fromJson(data); // works but not required
       $scope.sentences = data;
       console.log(typeof($scope.sentences));
+
+      // pre-process HTTP response by limiting sentences to first 3 on the list
+      $scope.sentences = data.splice(0, 3);
     }).
     error(function(data, status, headers, config) {
       // called asynchronously if an error occurs
@@ -41,9 +44,6 @@ angular.module('YeomanIonic.controllers', [])
     });
 
   // dataset moved to data/sentences.json
-
-  // pre-process HTTP response by limiting sentences to first 3 on the list
-  $scope.sentences = data.splice(0, 3);
 
   // default value of orderProp set so initialised in drop-down when used
   $scope.orderProp = 'snippet';
