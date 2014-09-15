c13ab221866763c1e3d7e1c01148b54e9f1a09ba
ltfschoen <ltfschoen@gmail.com>
2014-09-16 08:27:56 +1000

    Attempt at ngRoute Implementation. Unsuccessful as page does not display

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,34 @@
 'use strict';
 
-angular.module('YeomanIonic', ['ionic', 'config', 'YeomanIonic.controllers', 'YeomanIonic.directives'])
+var yeomanIonicApp = angular.module('yeomanIonicApp', [
+  'ionic', 
+  'config', 
+  'ngRoute',
+  'yeomanIonicControllers', 
+  'yeomanIonicDirectives'
+]);
 
-.run(function($ionicPlatform) {
+yeomanIonicApp.config(['$routeProvider',
+  function($routeProvider) {
+    // define our routes
+    $routeProvider.
+      when('/sentences', {
+        templateUrl: 'partials/sentence-list.html',
+        controller: 'MapCtrl'
+      }).
+      // :sentenceId variable extracted into $routeParams object
+      when('/sentences/:sentenceId', {
+        templateUrl: 'partials/sentence-detail.html',
+        controller: 'MapDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/sentences'
+      }
+    );
+  }
+]);
+
+yeomanIonicApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,3 +41,42 @@ angular.module('YeomanIonic', ['ionic', 'config', 'YeomanIonic.controllers', 'Ye
     }
   });
 });
+
+// OLD SYNTAX
+
+// angular.module('YeomanIonic', ['ionic', 'config', 'ngRoute', 'YeomanIonic.controllers', 'YeomanIonic.directives'])
+
+// // inject $routeProvider into config fn
+// .config(['$routeProvider',
+//   function($routeProvider) {
+//     // define our routes
+//     $routeProvider.
+//       when('/sentences', {
+//         templateUrl: 'partials/sentence-list.html',
+//         controller: 'MapCtrl'
+//       }).
+//       // :sentenceId variable extracted into $routeParams object
+//       when('/sentences/:sentenceId', {
+//         templateUrl: 'partials/sentence-detail.html',
+//         controller: 'MapDetailCtrl'
+//       }).
+//       otherwise({
+//         redirectTo: '/sentences'
+//       }
+//     );
+//   }
+// ]);
+
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//     // for form inputs)
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//     }
+//     if(window.StatusBar) {
+//       // org.apache.cordova.statusbar required
+//       window.StatusBar.styleDefault();
+//     }
+//   });
+// });
