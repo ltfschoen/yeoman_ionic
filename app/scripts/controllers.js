77b8cd485fc3922ba4d897f0840618b2289088f7
ltfschoen <ltfschoen@gmail.com>
2014-09-16 09:49:19 +1000

    Attempted to Add Close Button

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,7 +2,7 @@
 
 var yeomanIonicControllers = angular.module('yeomanIonicControllers', [])
 
-yeomanIonicControllers.controller('MapCtrl', ['$scope', '$ionicLoading', '$http', function($scope, $ionicLoading, $http) {
+yeomanIonicControllers.controller('MapCtrl', ['$scope', '$ionicLoading', '$http', '$timeout', function($scope, $ionicLoading, $http, $timeout) {
 
   // $http service for HTTP request to fetch data from web server asyncronously
   // AngularJS detects and parses the JSON response automatically
@@ -65,92 +65,29 @@ yeomanIonicControllers.controller('MapCtrl', ['$scope', '$ionicLoading', '$http'
       alert('Unable to get location: ' + error.message);
     });
   };
+
+  $scope.hideDialog = function () {
+    $scope.dialogIsHidden = true;
+    $timeout(function () {
+      $scope.dialogIsHidden = false;
+    }, 2000);
+  };
+
 }]);
+
+yeomanIonicControllers.directive('myDialog', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      'close': '&onClose'
+    },
+    templateUrl: 'shared/my-dialog-close.html'
+  };
+});
 
 yeomanIonicControllers.controller('MapDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 
   $scope.sentenceId = $routeParams.sentenceId;
 
 }]);
-
-// OLD SYNTAX
-
-// angular.module('YeomanIonic.controllers', [])
-
-// // AngularJS looks up its built-in services (prefixed with $) provided as arguments
-// // as dependencies when controller constructed. Dependency Injection (DI) applies
-// // the singleton instance of $http service to controller component
-// // DI checks if Service Instance of $http already instantiated
-// // else otherwise use Service Factory method fn for $http to construct it.
-// // inline annotation array of services to inject for corresponding parameter 
-// // to overcome problem with dependencies otherwise being minified incorrectly
-// .controller('MapCtrl', ['$scope', '$ionicLoading', '$http', function($scope, $ionicLoading, $http) {
-
-//   // $http service for HTTP request to fetch data from web server asyncronously
-//   // AngularJS detects and parses the JSON response automatically
-//   // $http service returns a promise object with a success method
-//   // assign dataset to scope of the controller as a Model named 'sentences'
-//   $http({
-//     method: 'GET',
-//     url: 'data/sentences.json'
-//   }).
-//     success(function(data, status, headers, config) {
-//       // this callback will be called asynchronously
-//       // when the response is available
-//       console.log('HTTP Request - Success');
-//       console.log('Data is: ' + data);
-//       console.log('Status is: ' + status);
-//       console.log('Headers is: ' + headers);
-//       console.log('Config is: ' + config);
-//       console.log('angular.fromJson(data) ==> ' + angular.fromJson(data));
-//       // console.log('eval(data) ==> ' + eval(data));  
-//       // $scope.sentences = eval(data); // security issues
-//       // $scope.sentences = angular.fromJson(data); // works but not required
-//       $scope.sentences = data;
-//       console.log(typeof($scope.sentences));
-
-//       // pre-process HTTP response by limiting sentences to first 4 on the list
-//       $scope.sentences = data.splice(0, 4);
-//     }).
-//     error(function(data, status, headers, config) {
-//       // called asynchronously if an error occurs
-//       // or server returns response with an error status.
-//       console.log('HTTP Request - Error');
-//       console.log('Data is: ' + data);
-//     });
-
-//   // dataset moved to data/sentences.json
-
-//   // default value of orderProp set so initialised in drop-down when used
-//   $scope.orderProp = 'snippet';
-  
-//   $scope.mapCreated = function(map) {
-//     $scope.map = map;
-//   };
-
-//   $scope.centerOnMe = function () {
-//     console.log('Centering');
-//     if (!$scope.map) {
-//       return;
-//     }
-
-//     $ionicLoading.show({
-//       content: 'Getting current location...',
-//       showBackdrop: false
-//     });
-
-//     navigator.geolocation.getCurrentPosition(function (pos) {
-//       console.log('Got pos', pos);
-//       $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-//       $ionicLoading.hide();
-//     }, function (error) {
-//       alert('Unable to get location: ' + error.message);
-//     });
-//   };
-// }]);
-
-// .controller('MapDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-
-//   $scope.sentenceId = $routeParams.sentenceId;
-
-// }]);
