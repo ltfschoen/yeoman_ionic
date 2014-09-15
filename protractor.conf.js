1042be6cfd78eeadc4221ce5d3c4cb40dc6a5665
ltfschoen <ltfschoen@gmail.com>
2014-09-15 15:21:50 +1000

    Changed to Standalone Selenium Server instead of Separate. Debugging Unit Tests.
    Ran the following:
    npm install --save-dev protractor
    npm install --save-dev chromedriver

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,16 +4,49 @@
 // http://stackoverflow.com/questions/23771922/protractor-error-message-unsupported-command-line-flag-in-chrome
 
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // OPTION #1 - PROTRACTOR TO START SELENIUM (STANDALONE MODE)
+  // (NO NEED TO RUN 'webdriver-manager start' as well, JUST RUN 'protractor protractor.conf.js')
+  // Run the following first:
+  // npm install --save-dev protractor
+  // npm install --save-dev chromedriver
+  chromeOnly: true,
+  chromeDriver: './node_modules/chromedriver/bin/chromedriver',
+  // OPTION #2 - RUN TESTS USING A SEPARATELY SELENIUM SERVER (FOR COMPLEX TESTS)
+  // (RUN WITH 'webdriver-manager start')
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // Enable multiCapabilities
+  // multiCapabilities: 
+  //   [{
+  //     'browserName': 'chrome',
+  //     'chromeOptions': {
+  //     args: ['--test-type']
+  //     }
+  //   },
+  //   {
+  //     'browserName': 'firefox'
+  //   }
+  // ],
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
         args: ['--test-type']
     }
   },
+  // http://ramonvictor.github.io/protractor/slides
+  onPrepare: function() {
+     browser.driver.manage().window().setSize(800, 600);
+  },
+  // Separate tests into test suites
+  // Run with: protractor protractor.conf.js --suite homepage
+  // suites: {
+  //   homepage: 'tests/e2e/**/*_test.js',
+  //   search: ['tests/e2e/contact_search/**/*Spec.js']
+  // },
   specs: ['./test/e2e/*_test.js'],
   baseUrl: 'http://localhost:9000', //default test port with Yeoman is 127.0.0.1 (localhost)
   jasmineNodeOpts: {
-    showColors: true
+    showColors: true,
+    isVerbose: true,
+    includeStackTrace: true
   }
 };
