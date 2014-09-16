71eaf989ad78cf457e9fb99ced43b0d1e19e8af9
ltfschoen <ltfschoen@gmail.com>
2014-09-16 18:24:41 +1000

    Added some experiments

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -2,12 +2,15 @@
 
 describe('filter', function() {
 
+  // call to load the filter module into the injector for this test run
   beforeEach(module('yeomanIonicFilters'));
 
   describe('checkmark', function() {
 
-    it('should convert boolean values to unicode checkmark or cross', inject(
-      function(checkmarkFilter) {
+    it('should convert boolean values to unicode checkmark or cross', 
+      // call helper function to access the filter we want to test
+      // suffix of 'Filter' is appended to filter name when injected
+      inject(function(checkmarkFilter) {
         expect(checkmarkFilter(true)).toBe('\u2713');
         expect(checkmarkFilter(false)).toBe('\u2718 ');
       }
