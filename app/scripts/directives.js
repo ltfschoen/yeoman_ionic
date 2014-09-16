5377ea7c17df983dd5291d849d78dbcfcd377e25
ltfschoen <ltfschoen@gmail.com>
2014-09-16 14:02:02 +1000

    Fixed Map display

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,32 +1,33 @@
 'use strict';
 
-angular.module('yeomanIonicDirectives', [])
+var yeomanIonicDirectives = angular.module('yeomanIonicDirectives', [])
 
-.directive('map', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      onCreate: '&'
-    },
-    link: function ($scope, $element) { // $attr param removed as not used
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(43.07493, -89.381388),
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map($element[0], mapOptions);
-  
-        $scope.onCreate({map: map});
+yeomanIonicDirectives.directive('map', 
+  function() {
+    return {
+      restrict: 'E',
+      scope: {
+        onCreate: '&'
+      },
+      link: function ($scope, $element) { // $attr param removed as not used
+        function initialize() {
+          var mapOptions = {
+            center: new google.maps.LatLng(43.07493, -89.381388),
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          var map = new google.maps.Map($element[0], mapOptions);
+    
+          $scope.onCreate({map: map});
 
-        // Stop the side bar from dragging when mousedown/tapdown on the map
-        google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
-          e.preventDefault();
-          return false;
-        });
+          // Stop the side bar from dragging when mousedown/tapdown on the map
+          google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+            e.preventDefault();
+            return false;
+          });
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
       }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
-    }
-  };
-});
+    };
+  }
+);
