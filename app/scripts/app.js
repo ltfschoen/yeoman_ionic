2cfc55e7f98137eccaa35b9430950dd5c7cf1f10
ltfschoen <ltfschoen@gmail.com>
2014-09-18 09:17:09 +1000

    Wrapped JS code in closures for best practice

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,49 @@
-'use strict';
+(function(){
 
-var yeomanIonicApp = angular.module('yeomanIonicApp', [
-  'ionic', 
-  'config', 
-  'ngRoute',
-  'yeomanIonicControllers', 
-  'yeomanIonicDirectives',
-  'yeomanIonicServices',
-  'yeomanIonicAnimations'
-]);
+  'use strict';
 
-yeomanIonicApp.config(['$routeProvider',
-  function($routeProvider) {
-    // define our routes
-    $routeProvider.
-      when('/sentences', {
-        templateUrl: 'partials/sentence-list.html',
-        controller: 'MapCtrl'
-      }).
-      // :sentenceId variable extracted into $routeParams object
-      when('/sentences/:sentenceId', {
-        templateUrl: 'partials/sentence-detail.html',
-        controller: 'MapDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/sentences'
+  var yeomanIonicApp = angular.module('yeomanIonicApp', [
+    'ionic', 
+    'config', 
+    'ngRoute',
+    'yeomanIonicControllers', 
+    'yeomanIonicDirectives',
+    'yeomanIonicServices',
+    'yeomanIonicAnimations'
+  ]);
+
+  yeomanIonicApp.config(['$routeProvider',
+    function($routeProvider) {
+      // define our routes
+      $routeProvider.
+        when('/sentences', {
+          templateUrl: 'partials/sentence-list.html',
+          controller: 'MapCtrl'
+        }).
+        // :sentenceId variable extracted into $routeParams object
+        when('/sentences/:sentenceId', {
+          templateUrl: 'partials/sentence-detail.html',
+          controller: 'MapDetailCtrl'
+        }).
+        otherwise({
+          redirectTo: '/sentences'
+        }
+      );
+    }
+  ]);
+
+  yeomanIonicApp.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
-    );
-  }
-]);
-
-yeomanIonicApp.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      window.StatusBar.styleDefault();
-    }
+      if(window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        window.StatusBar.styleDefault();
+      }
+    });
   });
-});
+
+})();
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,60 +1,64 @@
-'use strict';
+(function(){
 
-var yeomanIonicControllers = angular.module('yeomanIonicControllers', [])
+  'use strict';
 
-yeomanIonicControllers.controller('MapCtrl', ['$scope', '$ionicLoading', 'Sentences', function($scope, $ionicLoading, Sentences) {
+  var yeomanIonicControllers = angular.module('yeomanIonicControllers', [])
 
-  // $resource ngResource service for RESTful
-  // assign dataset to scope of the controller as a Model named 'sentences'
-  // 'Sentences.query()' GET request to 'data/sentences.json'
-  // relies on future object and data-binding to query all sentences
-  $scope.sentences = Sentences.query();
+  yeomanIonicControllers.controller('MapCtrl', ['$scope', '$ionicLoading', 'Sentences', function($scope, $ionicLoading, Sentences) {
 
-  // dataset moved to data/sentences.json
+    // $resource ngResource service for RESTful
+    // assign dataset to scope of the controller as a Model named 'sentences'
+    // 'Sentences.query()' GET request to 'data/sentences.json'
+    // relies on future object and data-binding to query all sentences
+    $scope.sentences = Sentences.query();
 
-  // default value of orderProp set so initialised in drop-down when used
-  $scope.orderProp = 'snippet';
-  
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
+    // dataset moved to data/sentences.json
 
-  $scope.centerOnMe = function () {
-    console.log('Centering');
-    if (!$scope.map) {
-      return;
-    }
+    // default value of orderProp set so initialised in drop-down when used
+    $scope.orderProp = 'snippet';
+    
+    $scope.mapCreated = function(map) {
+      $scope.map = map;
+    };
 
-    $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $ionicLoading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
-}]);
-
-// ngResource (instead of $http) service used to fetch JSON files
-yeomanIonicControllers.controller('MapDetailCtrl', [
-  '$scope', 
-  '$routeParams', 
-  'Sentence', 
-  function($scope, $routeParams, Sentence) {
-    // 'Sentence.get()' GET request to 'data/sentences/hello.json'
-    // '.get' instead of just '.query' adds a callback to process the server response
-    $scope.sentence = Sentence.get({sentenceId: $routeParams.sentenceId}, function(sentence) {
-      $scope.mainImageUrl = sentence.images[0]; // default image
-  
-      // event handler to change value of default main image
-      $scope.setImage = function(imageUrl) {
-        $scope.mainImageUrl = imageUrl;
+    $scope.centerOnMe = function () {
+      console.log('Centering');
+      if (!$scope.map) {
+        return;
       }
-    });
-  }
-]);
+
+      $ionicLoading.show({
+        content: 'Getting current location...',
+        showBackdrop: false
+      });
+
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        console.log('Got pos', pos);
+        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        $ionicLoading.hide();
+      }, function (error) {
+        alert('Unable to get location: ' + error.message);
+      });
+    };
+  }]);
+
+  // ngResource (instead of $http) service used to fetch JSON files
+  yeomanIonicControllers.controller('MapDetailCtrl', [
+    '$scope', 
+    '$routeParams', 
+    'Sentence', 
+    function($scope, $routeParams, Sentence) {
+      // 'Sentence.get()' GET request to 'data/sentences/hello.json'
+      // '.get' instead of just '.query' adds a callback to process the server response
+      $scope.sentence = Sentence.get({sentenceId: $routeParams.sentenceId}, function(sentence) {
+        $scope.mainImageUrl = sentence.images[0]; // default image
+    
+        // event handler to change value of default main image
+        $scope.setImage = function(imageUrl) {
+          $scope.mainImageUrl = imageUrl;
+        }
+      });
+    }
+  ]);
+
+})();
